64e56aec12fa60886f852769ffcf7e27ec13f9f3
Jun, Jin Soo <jjs048@gmail.com>
2021-11-06 16:45:55 +0900

    Student1 MyClass1 commit

---
+++ src/Student01/MyClass1.java
@@ -0,0 +1,13 @@
+package Student01;
+
+public class MyClass1 {
+
+    void myMethod1(){
+        System.out.println("myMethod1");
+    }
+
+    public static void main(String[] args) {
+        MyClass1 myMyClass1 = new MyClass1();
+        myMyClass1.myMethod1();
+    }
+}
