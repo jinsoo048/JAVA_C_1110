c2b6546a4808d0f66bd828256ea638daa6520076
Jun, Jin Soo <jjs048@gmail.com>
2021-11-13 16:44:41 +0900

    first commit

--- src/student01/HelloWorld.java
+++ src/student01/HelloWorld.java
@@ -1,9 +1,9 @@
-package JJ;
+package student01;
 
 public class HelloWorld {
 
     void myPrint(){
-          System.out.println("Hello JAVA World!");
+        System.out.println("Hello JAVA World!");
     }
     public static void main(String[] args) {
         HelloWorld myHelloWorld = new HelloWorld();
